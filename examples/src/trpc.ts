abdeb53ec6b8ab12ea8a6cfa6341662b3885de02
toyamarinyon <toyamarinyon@gmail.com>
2022-12-20 17:41:01 +0900

    delete old example

--- examples/src/trpc.ts
+++
@@ -1,4 +0,0 @@
-import { createReactQueryHooks } from '@trpc/react';
-import type { AppRouter } from '../functions/api/[[trpc]]';
-
-export const trpc = createReactQueryHooks<AppRouter>();
--- examples/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly CF_PAGES_URL: string?;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
